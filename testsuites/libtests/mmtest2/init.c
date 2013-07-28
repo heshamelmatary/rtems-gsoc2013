4cb55c6c5a5a5a9e6af5f9ab37ee1fbc43cbfb39
Hesham AL-Matary <heshamelmatary@gmail.com>
2013-07-28 04:14:29 +0200

    Refactor mmtest2 to sync with new libmm prototypes

--- testsuites/libtests/mmtest2/init.c
+++ testsuites/libtests/mmtest2/init.c
@@ -27,49 +27,31 @@ rtems_task Init(
   unsigned char* a2;
   char a;
 
-  /* FIXME: Make these addresses target-independent */
-  Memory_management_Entry mme1 = {
-    .name = "Valid Entry-1",
-    .base = 0x0C000000U,
-    .size = 0x1000U,
-    .installed = false,
-    .bsp_mme = NULL
-  };
-  Memory_management_Entry mme2 = {
-    .name = "Valid Entry-2",
-    .base = 0x0C100000U,
-    .size = 0x1000U,
-    .installed = false,
-    .bsp_mme = NULL
-  };
-
   puts( "\n\n*** Start of mmtest2 ***\n" );
 
-  //_Memory_management_Initialize ( );
-
   printf("Test 1: Installing Entry-1 and set it as read only\n");
-  _Memory_management_Install_entry( &mme1, RTEMS_MM_REGION_PROTECTION_READ_ONLY );
+  _Memory_management_Install_entry( 0x0C000000U, 0x1000U, RTEMS_MM_REGION_PROTECTION_READ_ONLY );
 
   printf("Test 2: Installing Entry-2 and set it as write enabled\n");
-  _Memory_management_Install_entry( &mme2, RTEMS_MM_REGION_PROTECTION_WRITE );
+  _Memory_management_Install_entry( 0x0C100000U, 0x1000U, RTEMS_MM_REGION_PROTECTION_WRITE );
 
   // TODO: Make fatal tests withing exception handler */
   /* FIXME: make addresses target-independent */
-  a1 = (char*)0xffffffffU;
 
-  a1 = mme2.base;
+  a1 = (char *) 0x0C100000U;
   printf("Test 3: Write to write enabled block\n");
   *a1 = 0xCC;
 
-  //printf("Checking MMU exception 1: Read from Unmapped block\n");
-  //a = *a1++;
+  a1 = (char*)0xffffffffU;
+  printf("Checking MMU exception 1: Read from Unmapped block\n");
+  a = *a1++;
 
-  //a1 = (char*)0xffffffffU + 0x2000U;
-  //printf("Checking MMU exception 2: Write to Unmapped block\n");
-  //*a1++ = 0xCC;
+  a1 = (char*)0xffffffffU + 0x2000U;
+  printf("Checking MMU exception 2: Write to Unmapped block\n");
+  *a1++ = 0xCC;
 
   // this one isn't an exception.
-  a2 = mme1.base;
+  a2 = (char *) 0x0C000000U;
   printf("Checking MMU exception 4: Read from readonly block\n");
   a = *a2++;
 
@@ -77,9 +59,9 @@ rtems_task Init(
   *a2++ = 0xCC;
 
   printf("Test 6: Uninstalling Entry-2 \n");
-  _Memory_management_Uninstall_entry( &mme2);
+  _Memory_management_Uninstall_entry(0x0C100000U, 0x1000U);
   
-  a1 = mme2.base;
+  a1 = (char *) 0x0C100000U;
   printf("Checking MMU exception 6: Write to uninstalled block \n");
   *a1 = 0xCC;
 
