0dc65b9084016d99529be897dd1a09282a10676f
Hesham AL-Matary <heshamelmatary@gmail.com>
2013-09-03 07:06:59 +0200

    change _CPU_xxx to bsb_xxx and move mm init routine to bsp files

---
+++ c/src/lib/libbsp/arm/shared/mminit.c
@@ -0,0 +1,23 @@
+/* 
+ * Copyright (c) 2013 Hesham AL-Matary.
+ * 
+ * The license and distribution terms for this file may be
+ * found in the file LICENSE in this distribution or at
+ * http://www.rtems.com/license/LICENSE.
+ */
+
+#include <bsp/mm_config_table.h>
+#include <bsp/linker-symbols.h>
+
+BSP_START_TEXT_SECTION void bsp_memory_management_Initialize(void)
+{
+  uint32_t ctrl = arm_cp15_get_control();
+
+  arm_cp15_start_setup_translation_table_and_enable_mmu_and_cache(
+  ctrl,
+  (uint32_t *) bsp_translation_table_base,
+  ARM_MMU_DEFAULT_CLIENT_DOMAIN,
+  &_cpu_mmu_config_table[0],
+  RTEMS_ARRAY_SIZE(&_cpu_mmu_config_table)
+  );
+}
